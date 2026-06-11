3f487d0e7762317bf2f807d2cd43d77632c105b9
Yasemin Türkoğlu <126945023+fizikzedenin-kodlari@users.noreply.github.com>
2026-06-11 10:17:49 +0300

    Add files via upload

---
+++ multi_rw_#5.lean
@@ -0,0 +1,6 @@
+import Mathlib.Tactic
+variable (a b : ℝ)
+example: (a + b) * (a + b) = a * a + 2 * (a * b) + b * b := by
+  rw [mul_add, add_mul, add_mul]
+  rw [← add_assoc, add_assoc (a * a)]
+  rw [mul_comm b a, ← two_mul]
